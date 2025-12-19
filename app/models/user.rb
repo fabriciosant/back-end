38462146f6f897a7914fb47d79329b03817b7f71
FabricioSeduc <fabriciosantos@seduc.ro.gov.br>
2025-12-19 10:22:15 -0400

    refactor: ajuste no envio de email duplicado, e otimizacao do html para o email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, :confirmable,
          jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
+
+
+  def send_on_create_confirmation_instructions
+  end
 end
